496676523dfb74502946d564900dffc551b3b412
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-15 21:50:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,15 +7,15 @@ def deal_card
 end
 
 def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}."
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  puts "Type 'h' to hit or 's' to stay."
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  gets
+  gets.chomp
 end
 
 def end_game
