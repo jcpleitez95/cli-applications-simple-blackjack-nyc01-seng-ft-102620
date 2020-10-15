0d67cb76aa18788b02e09cfc3cf396fe16434ff8
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-15 21:41:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,9 @@ def deal_card
   rand 1..11
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  card_total = deal_card
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
